3d7e1f389d7beb4dd87b38ab8c437253f5c7a866
jk <jk.hao@pku.edu.cn>
2020-01-19 21:25:50 +0800

    抄书上的eval

--- tsh.c
+++ tsh.c
@@ -165,6 +165,32 @@ int main(int argc, char **argv)
 */
 void eval(char *cmdline) 
 {
+    char *argv[MAXARGS];
+    char buf[MAXLINE];
+    int bg;
+    pid_t pid;
+
+    strcpy(buf, cmdline);
+    bg = parseline(buf, argv);
+    if(argv[0] == NULL) 
+        return;
+
+    if(!builtin_cmd(argv)) {
+        if((pid = fork()) == 0) {
+            if(execve(argv[0], argv, environ) < 0) {
+                printf("%s: Command not found.\n", argv[0]);
+                exit(0);
+            }   
+        }
+        if(!bg) {
+            int status;
+            if(waitpid(pid, &status, 0) < 0)
+                unix_error("waitfg: waitpid error");
+        }
+        else
+            printf("%d %s", pid, cmdline);
+    }
+
     return;
 }
 
@@ -231,6 +257,13 @@ int parseline(const char *cmdline, char **argv)
  */
 int builtin_cmd(char **argv) 
 {
+    if(strcmp(argv[0], "quit") == 0)
+        exit(0);
+    // if(strcmp(argv[0], "jobs") == 0) {
+    //     listjobs(jobs);
+        return 1;
+    }
+        
     return 0;     /* not a builtin command */
 }
 
